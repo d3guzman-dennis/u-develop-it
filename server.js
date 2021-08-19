58ab98222b273b0f30a327a9a09f75f73a63c3fb
Dennis de Guzman <84108180+d3guzman-dennis@users.noreply.github.com>
2021-08-19 18:43:52 -0400

    init sql

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '94Fynn08Meiji45Ori0!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+}); 
+  
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
